f00c23cafad081884acfac74e76075db36f74da3
xikaiqi <xikaiqi@insta360.com>
2022-01-10 18:52:13 +0800

    add src

---
+++ src/hello.cpp
@@ -0,0 +1,12 @@
+//
+// Created by insta360 on 2022/1/10.
+//
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    Sleep(3 * 1000);
+    return 0;
+}
+
